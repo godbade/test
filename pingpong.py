f888f3e80a90e453f369c8346ed9934448785cdc
set1ch <104989069+set1ch@users.noreply.github.com>
2022-05-05 18:12:05 +0500

    Update pingpong.py

--- pingpong.py
+++ pingpong.py
@@ -8,7 +8,10 @@ class gameobject(sprite.Sprite):
         sprite.Sprite.__init__(self)
         self.image = transform.scale(image.load(img), (q,q1))
         self.rect = self.image.get_rect()
-
+        self.rect.x = x
+        self.rect.y = y
+    def draw(self):
+        okno.blit(self.image, (self.rect.x, self.rect.y))
 while game:
     for i in event.get():
         if i.type == QUIT:
