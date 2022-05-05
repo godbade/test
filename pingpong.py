093fdaad4308effc70ce6997b34aeae5fbdab31f
godbade <104989013+godbade@users.noreply.github.com>
2022-05-05 16:09:16 +0300

    Update pingpong.py

--- pingpong.py
+++ pingpong.py
@@ -3,6 +3,11 @@ okno = display.set_mode((600,600))
 game = True
 clock = time.Clock()
 
+class gameobject(sprite.Sprite):
+    def __init__(self, img, x,y,q,q1):
+        sprite.Sprite.__init__(self)
+        self.image = transform.scale(image.load(img), (q,q1))
+        self.rect = self.image.get_rect()
 
 while game:
     for i in event.get():
