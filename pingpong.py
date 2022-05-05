5d71457fa05715688eed705b5a4dcce97cf45d98
godbade <104989013+godbade@users.noreply.github.com>
2022-05-05 16:02:13 +0300

    Update pingpong.py

--- pingpong.py
+++ pingpong.py
@@ -3,11 +3,10 @@ okno = display.set_mode((600,600))
 game = True
 clock = time.Clock()
 
-game = True 
 
 while game:
     for i in event.get():
         if i.type == QUIT:
             game = False
  
-display.update()
+    display.update()
