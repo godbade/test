7a48fb8d7f9f1848a274a77807f4cd06fd18a173
set1ch <104989069+set1ch@users.noreply.github.com>
2022-05-05 18:13:26 +0500

    Update pingpong.py

--- pingpong.py
+++ pingpong.py
@@ -12,6 +12,15 @@ class gameobject(sprite.Sprite):
         self.rect.y = y
     def draw(self):
         okno.blit(self.image, (self.rect.x, self.rect.y))
+        
+    
+class player(gameobject):
+    def control(self):
+        knopki = key.get_pressed()
+        if knopki[K_d] and self.rect.x < 450:
+            self.rect.x += 1
+        if knopki[K_a] and self.rect.x > 0:
+            self.rect.x -= 1    
 while game:
     for i in event.get():
         if i.type == QUIT:
